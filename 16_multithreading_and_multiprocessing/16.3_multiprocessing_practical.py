6495cabf757a151bcc54548a9ecb807279df413f
kumarjhamishra <03satyam05@gmail.com>
2026-09-08 21:59:51 +0530

    multithreading and multiprocessing

---
+++ 16_multithreading_and_multiprocessing/16.3_multiprocessing_practical.py
@@ -0,0 +1,33 @@
+# allows to create process run in parallel
+# for CPU bound task 
+# parallel exectuion for all core utilisation
+import multiprocessing
+import time
+
+def square_numbers():
+    for i in range(5):
+        time.sleep(1)
+        print(f"Square: {i*i}")
+
+def cube_numbers():
+    for i in range(5):
+        time.sleep(1.5)
+        print(f"Cube: {i*i*i}")
+
+
+if __name__ == "__main__":
+    # create 2 processes
+    p1 = multiprocessing.Process(target=square_numbers)
+    p2 = multiprocessing.Process(target=cube_numbers)
+
+    t = time.time()
+    # start the process
+    p1.start()
+    p2.start()
+
+    # wait for the process to complete
+    p1.join()
+    p2.join()
+
+    finished_time = time.time() - t
+    print(f"Time taken is: {finished_time}")
